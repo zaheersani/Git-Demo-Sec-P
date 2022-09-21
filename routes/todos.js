b701fc5e05718678b6b62a8e0a801b5013126d7b
zaheersani <sani.zaheer@gmail.com>
2022-09-21 10:14:36 +0500

    TODO route file updated

--- routes/todos.js
+++ routes/todos.js
@@ -3,22 +3,7 @@ import express from 'express';
 // const express = require('express');
 const router = express.Router();
 
-const todos = [
-    {
-        item: 'Push the code',
-        assignTo: 45
-    }
-]
-
-router.get('/', (req, res) => {
-    res.send(todos)
-})
-
-router.post('/', (req, res) => {
-    const user = req.body;
-    console.log(req.body);
-    res.send(`added into the DB`)
-})
+// provide todo route implementation here
 
 export default router;
 
